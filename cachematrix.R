52e5a076cb3f0c852be8813eed914d20b0facd0c
Heimo Laukkanen <huima@iki.fi>
2014-11-23 21:59:06 +0200

    This is the assignment commit

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,39 @@
-## Put comments here that give an overall description of what your
-## functions do
+## This is programming exercise that demonstrates how
+## computationally expensive function results can be
+## cached and reused
+##
+## makeCacheMatrix creates cache object that then gets
+## used in cacheSolve function, demonstrating how
+## to use the cache object
 
-## Write a short comment describing this function
+## makeCacheMatrix returns a cache object 
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  cache <- NULL
+  set <- function(y) {
+    x <<- y
+    cache <<- NULL ## clear the cache!
+  }
+  get <- function() x
+  setinverse <- function(inversematrix) cache <<- inversematrix
+  getinverse <- function() cache
+  list(set = set, get = get,
+       setinverse = setinverse,
+       getinverse = getinverse)
 }
 
 
-## Write a short comment describing this function
+## cacheSolve uses cache object to either return cached result or 
+## calculate it and then store the result in the cache
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  cachedata <- x$getinverse()
+  if(!is.null(cachedata)) {
+    message("getting cached data")
+    return(cachedata)
+  }
+  data <- x$get()
+  invmatrix <- solve(data, ...)
+  x$setinverse(invmatrix)
+  invmatrix
 }
